da1c641c647a6efb5ce058ff59f6c80f12c602fc
katrina b <iamkatmlb@gmail.com>
2024-06-14 16:39:23 +0200

    adding a new page for guided tour.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import VRTour from './components/VRTour';
+import GuidedTour from './components/GuidedTour';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/vrtour" element={<VRTour />} />
+      <Route path="/guided-tour" element={<GuidedTour />} />
     </Routes>
   );
 }
