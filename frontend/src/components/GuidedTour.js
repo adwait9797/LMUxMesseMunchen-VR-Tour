dbb296c20d21953fcfe9937fa05aada9612743f5
katrina b <iamkatmlb@gmail.com>
2024-06-14 16:28:29 +0200

    add a new page

---
+++ frontend/src/components/GuidedTour.js
@@ -0,0 +1,12 @@
+// GuidedTour.js
+import React from 'react';
+
+const GuidedTour = () => {
+  return (
+    <div>
+      Welcome to the Guided Tour Page
+    </div>
+  );
+};
+
+export default GuidedTour;
