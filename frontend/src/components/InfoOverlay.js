1f8cdf7d3b91a0fa7916687c6490ea0aaf1b6ddf
BERTOLDI, Mattia <Mattia.Bertoldi-extern@messe-muenchen.de>
2024-06-19 19:30:25 +0200

    chore: Add InfoOverlay component and styles

---
+++ frontend/src/components/InfoOverlay.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './InfoOverlay.css';
+import closeIcon from './assets/close_icon.svg';
+
+const InfoOverlay = ({ roomInfo, onClose }) => {
+  return (
+    <div className="info-overlay">
+      <div className="close-overlay" onClick={onClose}>
+        <img src={closeIcon} alt="Close" />
+      </div>
+      <div className="overlay-content">
+        <h2>{roomInfo.title}</h2>
+        <p>{roomInfo.description}</p>
+        {/* Add more room details here if needed */}
+      </div>
+    </div>
+  );
+};
+
+export default InfoOverlay;
