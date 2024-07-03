193fc5f305096deb513bdf9f160489d645a7a5e2
BERTOLDI, Mattia <Mattia.Bertoldi-extern@messe-muenchen.de>
2024-07-03 17:23:12 +0200

    chore: Update HotspotOverlay component with new image and styles

--- frontend/src/components/HotspotOverlay.js
+++ frontend/src/components/HotspotOverlay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'aframe';
 import './Hotspot.css';
+import hotspotImage from './assets/Rectangle_hotspot.png';
 
 const HotspotOverlay = ({ hotspot, onClose }) => {
   const [x, y, z] = hotspot.position.split(' ').map(Number);
@@ -8,56 +9,64 @@ const HotspotOverlay = ({ hotspot, onClose }) => {
 
   return (
     <a-entity id="hotspotOverlay" position={adjustedPosition}>
-      <a-plane
-        width="6" /* Increased width */
-        height="4" /* Increased height */
-        material="color: #ffffff; opacity: 0.95; shader: flat"
+      <a-image
+        src={hotspotImage}
+        width="4"
+        height="3"
         look-at="#camera"
         class="hotspot-overlay-background"
       >
-        <a-text
+          <a-plane
+              width="3.8"
+              height="2.8"
+              material="color: #ffffff; transparent = true"
+              position="0 0 0.005"
+          ></a-plane>
+          <a-text
           value={hotspot.title}
-          color="#000000" 
+          color="#000000"
           align="center"
-          position="0 1.5 0.01" 
-          width="5"
+          position="0 1.2 0.01"
+          width="3.5"
           font="kelsonsans"
           shader="msdf"
           negate="false"
+          scale="1.2 1.2 1"
           class="hotspot-overlay-title"
-          height="1" 
         ></a-text>
+
         <a-text
           value={hotspot.description}
           color="#333333"
           align="center"
-          position="0 0.5 0.01"
-          width="5"
+          position="0 0.2 0.01"
+          width="3.5"
           font="kelsonsans"
           shader="msdf"
           negate="false"
+          scale="0.8 0.8 1"
           class="hotspot-overlay-description"
-          height="0.8" 
+          wrap-count="30"
         ></a-text>
-        <a-circle
-          radius="0.6"
-          position="0 -1.7 0.01" /* Adjusted position */
-          material="color: #333333; opacity: 0.8"
-          class="clickable hotspot-overlay-close"
-          onClick={onClose}
-        >
-          <a-text
-            value="Close"
-            color="#ffffff"
-            align="center"
-            position="0 0 0.01"
-            width="4"
-            font="kelsonsans"
-            shader="msdf"
-            negate="false"
-          ></a-text>
-        </a-circle>
-      </a-plane>
+        <a-entity
+        geometry="primitive: circle; radius: 0.6"
+        material="color: #4a4a4a; opacity: 0.9"
+        position="0 -1.3 0.02"
+        class="clickable hotspot-overlay-close"
+        onClick={onClose}
+      >
+      <a-text
+        value="Close"
+        color="#ffffff"
+        align="center"
+        position="0 0 0.01"
+        scale="0.8 0.8 1"
+        font="kelsonsans"
+        shader="msdf"
+        negate="false"
+      ></a-text>
+    </a-entity>
+      </a-image>
     </a-entity>
   );
 };
